3ae3e9e9f33b1f17ff31126979b6ce9249e0043d
Dedy Setiawan <dedisetiawan34@gmail.com>
2019-06-06 01:06:49 +0800

    fix focus and color on select2

--- application/views/pengadaan/pekerjaan/pekerjaan_form.php
+++ application/views/pengadaan/pekerjaan/pekerjaan_form.php
@@ -60,7 +60,7 @@
 						<td><label for="decimal">Nilai Kontrak <?php echo form_error('longitude') ?></label></td>
 						<td><input type="number" class="form-control" name="longitude" id="longitude" placeholder="koordinat garis longitude" value="<?php echo $longitude; ?>" step='any' min=0/></td>
 					</tr>
-					
+
 					<input type="hidden" name="id" value="<?php echo $id_p; ?>" />
 					<tr>
 						<td colspan='2'>
@@ -79,6 +79,12 @@
 <script type="text/javascript">
 	$(document).ready(function () {
 		$("#nama").focus();
+		$('select').on(
+    'select2:close',
+    function () {
+        $(this).focus();
+    }
+		);
 		$('#skpd').select2({
 			placeholder: "Pilih SKPD",
 			allowClear:	true,
